e5ac554cec05c704ba92f38d1b51d8acce928f79
Khadija Zayan <khadija.mzayan@gmail.com>
2024-11-07 21:36:08 +0300

    Books

---
+++ Books.py
@@ -0,0 +1,34 @@
+library = []
+wishlist = []
+
+book_name = input ("Enter the name of a book you own: ")
+library.append(book_name)
+
+book_name = input ("Enter the name of another book you own (or press 'Enter' to skip): ")
+if book_name:
+    library.append(book_name)
+
+print ("\nYour Library: ", library)
+
+book_name = input ("\nEnter the name of a book you wish to have in the future: ")
+wishlist.append(book_name)
+book_name = input ("Enter the name of another book you wish to have (or press 'Enter' to skip): ")
+if book_name:
+  wishlist.append(book_name)
+
+print ("\nYour Wishlist: ", wishlist)
+
+acquired_book = input ("\nEnter the name of a book from your wishlist that you've acquired (or press 'Enter' to skip): ")
+if acquired_book in wishlist:
+  library.append(acquired_book)
+  wishlist.remove(acquired_book)
+
+print ("\nUpdated Library: ", library)
+print ("Updated Wishlist: ", wishlist)
+
+donated_book = input ("\nEnter the name of a book from your library you wish to donate (or press 'Enter' to skip): ")
+if donated_book in library:
+  library.remove(donated_book)
+
+print ("\nFinal Library after Donations: ", library)
+print ("\nFinal Wishlist after Donations: ", wishlist)
