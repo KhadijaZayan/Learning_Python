560215b5473b7ca718d9ae82a7ad7870ed1e83d2
Khadija Zayan <khadija.mzayan@gmail.com>
2024-11-07 21:46:16 +0300

    Place-the-rabbit

---
+++ Place-the-rabbit.py
@@ -0,0 +1,49 @@
+print("Welcome to (place the rabbit) game!!!")
+print(" ")
+grass1 = ['🌿', '🌿', '🌿', '👨🏻🌾']
+grass2 = ['🌿', '🌿', '🌿', '🌿']
+grass3 = ['🐇', '🌿', '🌿', '🌿']
+print(grass1)
+print(grass2)
+print(grass3)
+print(" ")
+print(" ") 
+print("Wait for a minute, please...")
+print(" ")
+grass_numder1 = [1, 2, 3, '👨🏻🌾']
+grass_numder2 = [ 4, 5, 6, 7   ]
+grass_numder3 = ['🐇', 8, 9, 10  ]
+print(grass_numder1)
+print(grass_numder2)
+print(grass_numder3)
+print(" ")
+
+print("Here is the heden (grass numder)")
+int_choice = input("So what number is the rabbit next to the farmer? ")
+if int_choice == "3":
+    print("⭐️Good job⭐️")
+    print(" ") 
+    print("success..")
+    print(" ")
+    f = ["🌿", "🌿", "🐇", "👨🏻🌾"]
+    c = ["🌿", "🌿", "🌿", "🌿"]
+    d = ["🌿", "🌿", "🌿", "🌿"]
+    print(f)
+    print(c)
+    print(d)
+else:
+  print(" ")
+  print("No, this is a wrong number😔.")
+  int_choice = input("Try again..So what is the number is the rabbit under the farmer? ") 
+  if int_choice == "7":
+    print("⭐️Good job⭐️") 
+    print("success..") 
+    print(" ")
+    q = ["🌿", "🌿", "🌿", "👨🏻🌾"]
+    w = ["🌿", "🌿", "🌿", "🐇"]
+    e = ["🌿", "🌿", "🌿", "🌿"]
+    print(q)
+    print(w)
+    print(e)
+  else:
+   print("Sorry, wrong number \nGoodbey👋🏻👋🏻")
